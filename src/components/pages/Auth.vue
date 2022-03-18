d0ae518a4109f6029dc9b25f054da550d4c0399b
Bala K Reddy <99819356+balareddy-ec@users.noreply.github.com>
2022-03-18 23:41:09 +0530

    Initial commit

---
+++ src/components/pages/Auth.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="auth">
+      <button @click="initoAuth">Auth</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Auth',
+
+  components: {
+  },
+
+  data() {
+    return {
+
+    };
+  },
+
+  methods: {
+
+      async initoAuth() {
+        console.log('sdsd', this.$route.query.code);
+        const response = await window.salesforceConnector.getAccessToken(this.$route.query.code);
+
+        console.log(response);
+      },
+
+  },
+
+}
+</script>
---
+++ src/connectors/salesforce/salesforceConnectorREST.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+export default class SalesforceConnectorREST {
+    /** ****************      Auth Start by Bala ******************   */
+    getoAuthCode(url, cKey, redirectUrl) {
+        window.location.replace(`${url}client_id=${cKey}&redirect_uri=${
+            encodeURI(redirectUrl)
+        }&response_type=code`);
+        // return new Promise((resolve, reject) => {
+        //     axios.get(
+        //         `${url}client_id=${cKey}&redirect_uri=${
+        //             encodeURI('https://login.salesforce.com/services/oauth2/callback')
+        //         }&response_type=code`
+        //     )
+        //     .then(r => {
+        //         resolve(r.data);
+        //     })
+        //     .catch(e => {
+        //         reject(e);
+        //     })
+
+        // });
+    }
+
+    /** ****************      Auth Start by Bala ******************   */
+    getAccessToken(code, redirect_uri) {
+        const data = {
+            grant_type: 'authorization_code',
+            code,
+            client_id: '3MVG9riCAn8HHkYWhCjpJsP6M7CogzJaVsFgPuLnx8ubjRUAWxfz2FwveQnigPmuOVM1p2khA_Mmqj03tqzjc',
+            client_secret: '25D3EAE8C9C5DDE04365A4BF7087115DAA6E0597F79A617A1D5730D7542B7BAF',
+            redirect_uri: '',
+        }
+        return new Promise((resolve, reject) => {
+            axios.post(
+                'https://ec-expedite-dev-ed.my.salesforce.com/services/oauth2/token',
+                data
+            )
+            .then(r => {
+                resolve(r.data);
+            })
+            .catch(e => {
+                reject(e);
+            })
+
+        });
+    }
+    /** ****************      Auth Stop by Bala ******************   */
+}
