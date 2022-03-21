feb50fe80bfdddc295f6b47fd6b8635e74e9c2a7
Bala K Reddy <99819356+balareddy-ec@users.noreply.github.com>
2022-03-21 23:03:28 +0530

    Unsafe header removed

--- src/connectors/salesforce/salesforceConnectorREST.js
+++ src/connectors/salesforce/salesforceConnectorREST.js
@@ -42,7 +42,6 @@ export default class SalesforceConnectorREST {
                     headers: {
                         crossorigin: true,
                         "Content-type": "application/x-www-form-urlencoded",
-                        "Content-length": 307
                     } 
                 }
             )
